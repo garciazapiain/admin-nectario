5611c6010b9e5b2d100f6c80b211dc87c556b8dd
garciazapiain <juangarciazapiain@gmail.com>
2024-02-14 15:22:43 -0600

    update

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,10 @@ const cors = require('cors');
 const { Pool } = require('pg');
 
 const app = express();
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'frontend/public')));
