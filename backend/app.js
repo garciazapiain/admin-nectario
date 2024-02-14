4f33c58879cb0824ee9923cde4ff322a42ee6daa
garciazapiain <juangarciazapiain@gmail.com>
2024-02-14 16:28:58 -0600

    changes

--- backend/app.js
+++ backend/app.js
@@ -10,11 +10,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'frontend/dist'))); // Change 'public' to 'dist'
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-// Add this route handler
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend/dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
 });
 
 const pool = new Pool({
