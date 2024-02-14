0a09e9348d143b7f3bbc6d202205595d57dc28b5
garciazapiain <juangarciazapiain@gmail.com>
2024-02-14 17:36:45 -0600

    check if still works

--- backend/app.js
+++ backend/app.js
@@ -12,9 +12,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+// });
 
 const pool = new Pool({
   host: 'localhost',
