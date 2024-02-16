bc017b8dfb7ab4132d898901e43ac6b89b544bc2
garciazapiain <juangarciazapiain@gmail.com>
2024-02-16 15:13:44 -0600

    express back res.sendFile

--- backend/app.js
+++ backend/app.js
@@ -436,9 +436,9 @@ app.get('/api/proveedores', async (req, res) => {
 
 
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
