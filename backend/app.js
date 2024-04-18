f83c9a4c6089fbdf5806037c448e01c862ef145a
garciazapiain <juangarciazapiain@gmail.com>
2024-04-18 10:14:56 -0600

    fix backend add ingredient

--- backend/app.js
+++ backend/app.js
@@ -405,7 +405,7 @@ app.post('/api/ingredientes', async (req, res) => {
   const { nombre, unidad, precio, proveedor, proveedor_id } = req.body;
   const client = await pool.connect();
   try {
-    const result = await client.query('INSERT INTO ingredientes (nombre, unidad, precio, proveedor, proveedor_id, store_route_order) VALUES ($1, $2, $3, $4, $5) RETURNING *', [nombre, unidad, precio, proveedor, proveedor_id]);
+    const result = await client.query('INSERT INTO ingredientes (nombre, unidad, precio, proveedor, proveedor_id) VALUES ($1, $2, $3, $4, $5) RETURNING *', [nombre, unidad, precio, proveedor, proveedor_id]);
     res.json(result.rows[0]);
   } catch (err) {
     console.error(err);
