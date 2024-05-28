f400ad61c735e8aa90596a37bc26ec4f01cc8119
garciazapiain <juangarciazapiain@gmail.com>
2024-05-28 12:58:55 -0600

    hide some components in home

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -52,16 +52,16 @@ onMounted(() => {
   <div class="button-container">
     <button @click="handleClickInventario">Toma Inventario</button>
     <button @click="handleClickExistencias">Resumen Existencias</button>
-    <button v-if="isAdmin" @click="handleClickCompra">Compra del dia</button>
+    <!-- <button v-if="isAdmin" @click="handleClickCompra">Compra del dia</button> -->
     <button v-if="isAdmin" @click="handleClickPlatillo">Platillos</button>
     <button v-if="isAdmin" @click="handleClickSubplatillo">Subplatillos</button>
-    <button v-if="isAdmin" @click="handleClickIngrediente">Insumos Info</button>
-    <button v-if="isAdmin" @click="handleClickPronosticoDemandaInsumos">
+    <button v-if="isAdmin" @click="handleClickIngrediente">Insumos Lista</button>
+    <!-- <button v-if="isAdmin" @click="handleClickPronosticoDemandaInsumos">
       Pronóstico Demanda Insumos
-    </button>
-    <button v-if="isAdmin" @click="handleClickHistorialCompra">
+    </button> -->
+    <!-- <button v-if="isAdmin" @click="handleClickHistorialCompra">
       Historial Compras
-    </button>
+    </button> -->
     <button v-if="isAdmin" @click="handleClickConsumoInsumos">
       Consumo Insumos
     </button>
