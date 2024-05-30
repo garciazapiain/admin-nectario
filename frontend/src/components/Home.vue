09b398cf013e49b45c120d2248a0e302635a3cbd
garciazapiain <juangarciazapiain@gmail.com>
2024-05-30 10:05:19 -0600

    workdlow modified, home refactored

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -5,40 +5,8 @@ import { useRouter } from "vue-router";
 const router = useRouter();
 const isAdmin = ref(localStorage.getItem("isAdmin") === "true");
 
-const handleClickPlatillo = () => {
-  router.push("/platillos");
-};
-
-const handleClickSubplatillo = () => {
-  router.push("/subplatillos");
-};
-
-const handleClickIngrediente = () => {
-  router.push("/ingredientes");
-};
-
-const handleClickInventario = () => {
-  router.push("/inventarios");
-};
-
-const handleClickExistencias = () => {
-  router.push("/existenciasresumen");
-};
-
-const handleClickCompra = () => {
-  router.push("/compra");
-};
-
-const handleClickPronosticoDemandaInsumos = () => {
-  router.push("/pronosticodemandainsumos");
-};
-
-const handleClickHistorialCompra = () => {
-  router.push("/historialcompra");
-};
-
-const handleClickConsumoInsumos = () => {
-  router.push("/consumoinsumos");
+const navigateTo = (route) => {
+  router.push(route);
 };
 
 onMounted(() => {
@@ -50,16 +18,16 @@ onMounted(() => {
 
 <template>
   <div class="button-container">
-    <button @click="handleClickInventario">Toma Inventario</button>
-    <button @click="handleClickExistencias">Resumen Existencias</button>
-    <!-- <button v-if="isAdmin" @click="handleClickCompra">Compra del dia</button> -->
-    <button v-if="isAdmin" @click="handleClickPlatillo">Platillos</button>
-    <button v-if="isAdmin" @click="handleClickSubplatillo">Subplatillos</button>
-    <button v-if="isAdmin" @click="handleClickIngrediente">Insumos Lista</button>
-    <!-- <button v-if="isAdmin" @click="handleClickPronosticoDemandaInsumos">
+    <button @click="() => navigateTo('/inventarios')">Toma Inventario</button>
+    <button @click="() => navigateTo('/existenciasresumens')">Resumen Existencias</button>
+    <!-- <button v-if="isAdmin" @click="() => navigateTo('/compra')">Compra del dia</button> -->
+    <button v-if="isAdmin" @click="() => navigateTo('/platillos')">Platillos</button>
+    <button v-if="isAdmin" @click="() => navigateTo('/subplatillos')">Subplatillos</button>
+    <button v-if="isAdmin" @click="() => navigateTo('/ingredientes')">Insumos Lista</button>
+    <!-- <button v-if="isAdmin" @click="() => navigateTo('/pronosticodemandainsumos')">
       Pronóstico Demanda Insumos
     </button> -->
-    <!-- <button v-if="isAdmin" @click="handleClickHistorialCompra">
+    <!-- <button v-if="isAdmin" @click="() => navigateTo('/historialcompra')">
       Historial Compras
     </button> -->
     <button v-if="isAdmin" @click="handleClickConsumoInsumos">
@@ -75,7 +43,8 @@ onMounted(() => {
 }
 
 .button-container button {
-  flex: 1 0 20%; /* This will make each button take up 25% of the container's width, allowing for 4 items per row */
+  flex: 1 0 20%;
+  /* This will make each button take up 25% of the container's width, allowing for 4 items per row */
   box-sizing: border-box;
   margin: 10px;
   padding: 10px;
