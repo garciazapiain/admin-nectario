4d8301f96f9aa750459c067e5ba41c77429d04ad
garciazapiain <juangarciazapiain@gmail.com>
2024-05-30 09:49:44 -0600

    Cypress added

---
+++ frontend/src/components/Existencias/ResumenExistencias.cy.js
@@ -0,0 +1,46 @@
+import ResumenExistencias from './ResumenExistencias.vue';
+
+describe('<ResumenExistencias />', () => {
+  beforeEach(() => {
+    cy.mount(ResumenExistencias)
+  })
+  it('renders the component', () => {
+    cy.get('h1').should('contain', 'Resumen Existencias Lista Peligro');
+    cy.get('.search-bar').should('exist');
+  });
+
+  it('shows, interacts with, and hides the popup', () => {
+    // Show the popup
+    cy.get('table tbody tr:first').click();
+    cy.get('.popup').should('be.visible');
+
+    // Test the increment button
+    cy.get('.popup .controls').find('button').contains('+').click();
+    cy.get('.popup input[type="number"]').should(($input) => {
+      expect(parseInt($input.val())).to.be.above(1);
+    });
+
+    // Test the decrement button
+    cy.get('.popup .controls').find('button').contains('-').click();
+    cy.get('.popup input[type="number"]').should(($input) => {
+      expect(parseInt($input.val())).to.equal(1);
+    });
+
+    // Test the input field
+    cy.get('.popup input[type="number"]').clear().type('5');
+    cy.get('.popup input[type="number"]').should(($input) => {
+      expect(parseInt($input.val())).to.equal(5);
+    });
+
+    // Hide the popup
+    cy.get('.popup').find('button').contains('Cerrar').click();
+    cy.get('.popup').should('not.be.exist');
+  });
+
+  it('filters ingredients by search term', () => {
+    cy.get('.search-bar').type('ATUN'); // replace 'test' with an actual ingredient name
+    cy.get('table tbody tr').each(($el) => {
+      cy.wrap($el).should('contain', 'ATUN'); // replace 'test' with the same ingredient name
+    });
+  });
+})
---
+++ frontend/src/components/Inventarios/TomaInventario.cy.js
@@ -0,0 +1,39 @@
+import TomaInventario from './TomaInventario.vue'
+
+describe('<TomaInventario />', () => {
+  beforeEach(() => {
+    cy.mount(TomaInventario, {
+      props: {
+        store: 'bosques'
+      }
+    })
+  })
+
+  it('renders the component', () => {
+    cy.get('h1').should('contain', 'Toma Inventario')
+  })
+
+  it('filters ingredients based on search term', () => {
+    cy.get('.search-bar').type('ATUN')
+    cy.get('tbody tr').each(($el) => {
+      cy.wrap($el).find('td:first').should('contain', 'ATUN')
+    })
+  })
+
+  it('updates inventory quantity by increasing and decreasing', () => {
+    cy.get('tbody tr:first').as('firstRow')
+    cy.get('@firstRow').find('.button-agotado').click()
+    cy.get('@firstRow').find('input').should('have.value', '0')
+    cy.get('@firstRow').find('.button-increase-decrease').eq(1).click()
+    cy.get('@firstRow').find('input').should('have.value', '0.5')
+    cy.get('@firstRow').find('.button-increase-decrease').eq(0).click()
+    cy.get('@firstRow').find('input').should('have.value', '0')
+  })
+
+  it('submits the inventory form', () => {
+    cy.intercept('POST', '/api/submissions').as('submitForm')
+    cy.get('.button-actualizar').click()
+    cy.wait('@submitForm').its('response.statusCode').should('eq', 200)
+    cy.get('.success-message').should('be.visible').and('contain', 'Actualización exitosa')
+  })
+})
