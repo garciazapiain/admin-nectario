b288d1d5f4c6f7af09c674d129e343967e9ffb7f
garciazapiain <juangarciazapiain@gmail.com>
2024-05-31 09:38:50 -0600

    refactor main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,58 +1,16 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue'
-import Home from './components/Home.vue';
-import Login from './components/Auth/Login.vue';
-import Register from './components/Auth/Register.vue';
-import Platillos from './components/Platillos/Platillos.vue';
-import Platillo from './components/Platillos/Platillo.vue';
-import Subplatillos from './components/Subplatillos/Subplatillos.vue';
-import Subplatillo from './components/Subplatillos/Subplatillo.vue';
-import Ingredientes from './components/Ingredientes/Ingredientes.vue';
-import Ingrediente from './components/Ingredientes/Ingrediente.vue';
-import Inventarios from './components/Inventarios/Home.vue';
-import TomaInventario from './components/Inventarios/TomaInventario.vue';
-import ResumenExistencias from './components/Existencias/ResumenExistencias.vue';
-import Compra from './components/Compra/Compra.vue';
-import PronosticoDemandaInsumos from './components/PronosticoDemandaInsumos/PronosticoDemandaInsumos.vue';
-import HistorialCompra from './components/HistorialCompra/HistorialCompra.vue';
-import InsumosHistorial from './components/HistorialCompra/InsumosHistorial.vue';
-import InsumoAnalisis from './components/HistorialCompra/InsumoAnalisis.vue';
-import CompraAnalisis from './components/HistorialCompra/CompraAnalisis.vue';
-import ConsumoInsumos from './components/ConsumoInsumos/ConsumoInsumos.vue';
-import CargarVenta from './components/ConsumoInsumos/CargarVentas.vue';
+import routes from './routes.js'
 import './style.css'
 
-const routes = [
-    { path: '/', component: Home },
-    { path: '/login', component: Login },
-    { path: '/register', component: Register },
-    { path: '/platillos', component: Platillos },
-    { path: '/platillo/:id', component: Platillo },
-    { path: '/subplatillos', component: Subplatillos },
-    { path: '/subplatillo/:id', component: Subplatillo },
-    { path: '/ingredientes', component: Ingredientes },
-    { path: '/ingrediente/:id', component: Ingrediente },
-    { path: '/inventarios', component: Inventarios },
-    { path: '/inventarios/:store', component: TomaInventario, props: true},
-    { path: '/existenciasresumen', component: ResumenExistencias},
-    { path: '/compra', component: Compra },
-    { path: '/pronosticodemandainsumos', component: PronosticoDemandaInsumos },
-    { path: '/historialcompra', component: HistorialCompra   },
-    { path: '/historialcompra/insumos', component: InsumosHistorial   },
-    { path: '/historialcompra/insumos/:id', component: InsumoAnalisis   },
-    { path: '/historialcompra/compra/:id', component: CompraAnalisis   },
-    { path: '/consumoinsumos', component: ConsumoInsumos   },
-    { path: '/consumoinsumos/cargarventa', component: CargarVenta   }
-  ];
-  
-  const router = createRouter({
+const router = createRouter({
     history: createWebHistory(),
     routes,
-  });
-  
-  const app = createApp(App);
-  
-  app.use(router);
-  
-  app.mount('#app');
+});
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount('#app');
