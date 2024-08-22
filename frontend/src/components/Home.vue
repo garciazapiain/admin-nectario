1a6382223e8deb0662aba272903e113ce9e83c4b
garciazapiain <juangarciazapiain@gmail.com>
2024-08-22 09:42:30 -0600

    pronostico

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -25,9 +25,9 @@ onMounted(() => {
     <button class="button" v-if="isAdmin" @click="() => navigateTo('/platillos')">Platillos</button>
     <button class="button" v-if="isAdmin" @click="() => navigateTo('/subplatillos')">Subplatillos</button>
     <button class="button" v-if="isAdmin" @click="() => navigateTo('/ingredientes')">Insumos</button>
-    <!-- <button class="button" v-if="isAdmin" @click="() => navigateTo('/pronosticodemandainsumos')">
+    <button class="button" v-if="isAdmin" @click="() => navigateTo('/pronosticodemandainsumos')">
       Pronóstico Demanda Insumos
-    </button> -->
+    </button>
     <button class="button" v-if="isAdmin" @click="() => navigateTo('/historialcompra')">
       Historial Compras
     </button>
