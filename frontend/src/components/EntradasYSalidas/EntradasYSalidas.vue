28251d47dd21d70a3515e98c1c71751a50cd9aa6
garciazapiain <juangarciazapiain@gmail.com>
2024-11-25 15:14:09 -0600

    ..

--- frontend/src/components/EntradasYSalidas/EntradasYSalidas.vue
+++ frontend/src/components/EntradasYSalidas/EntradasYSalidas.vue
@@ -61,7 +61,7 @@ const updateDateRange = () => {
 
 const formatDateToLocal = (date) => {
   const adjustedDate = new Date(date.getTime() - date.getTimezoneOffset() * 60 * 1000);
-  adjustedDate.setDate(adjustedDate.getDate()); // Adjust one day forward
+  adjustedDate.setDate(adjustedDate.getDate() + 1); // Adjust one day forward
   console.log('adjusted date', adjustedDate.toISOString().split('T')[0])
   return adjustedDate.toISOString().split('T')[0];
 };
