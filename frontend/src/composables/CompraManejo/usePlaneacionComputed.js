92549f72c2dce306d684afe86516a094560933bb
garciazapiain <juangarciazapiain@gmail.com>
2025-03-07 14:14:43 -0600

    compra module - fixed bug, should appear when its 0 literally written

--- frontend/src/composables/CompraManejo/usePlaneacionComputed.js
+++ frontend/src/composables/CompraManejo/usePlaneacionComputed.js
@@ -11,13 +11,13 @@ export default function usePlaneacionComputed(planeacionCompra) {
 
   const moralOrders = computed(() =>
     planeacionCompra.value
-      .filter((ingrediente) => ingrediente.surtir_moral && ingrediente.surtir_moral !== "0")
+      .filter((ingrediente) => ingrediente.surtir_moral != null && ingrediente.surtir_moral !== "")
       .sort((a, b) => (a.ya_entregado_moral === b.ya_entregado_moral ? 0 : a.ya_entregado_moral ? 1 : -1))
   );
 
   const bosquesOrders = computed(() =>
     planeacionCompra.value
-      .filter((ingrediente) => ingrediente.surtir_campestre && ingrediente.surtir_campestre !== "0")
+      .filter((ingrediente) => ingrediente.surtir_campestre != null && ingrediente.surtir_campestre !== "")
       .sort((a, b) => (a.ya_entregado_bosques === b.ya_entregado_bosques ? 0 : a.ya_entregado_bosques ? 1 : -1))
   );
 
